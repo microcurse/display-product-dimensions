eeecf13fa3c273412a744352c41dbf94cd9047e5
Marc Maninang <marc.maninang@gmail.com>
2023-10-13 10:32:54 -0700

    Initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# WooCommerce Display Product Dimensions
+
+This plugin is for displaying WooCommerce product dimensions under the product's short description.
---
+++ display-product-dimensions.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+Plugin Name: WooCommerce Product Dimensions Table
+Description: Displays a WooCommerce Product's dimensions in a table under the short description of the product details page.
+Version: 1.0.0
+Author: Marc M.
+*/
+
+// Register a hook to display the product dimensions table under the short description.
+add_action('woocommerce_single_product_summary', 'wc_product_dimensions_table', 25);
+add_action( 'woocommerce_single_product_summary', 'wc_product_dimensions_enqueue_style' );
+
+function wc_product_dimensions_enqueue_style() {
+  wp_enqueue_style( 'wc-product-dimensions-table', plugins_url( 'display-product-dimensions/css/wc-product-dimensions-table.css', dirname( __FILE__ ) ) );
+}
+
+/**
+ * Displays a WooCommerce Product's dimensions in a table under the short description of the product details page.
+ *
+ * @param WC_Product $product The product object.
+ */
+function wc_product_dimensions_table($product) {
+  global $product;
+    // Get the product dimensions.
+  $formatted = false;
+  $dimensions = $product->get_dimensions($formatted);
+
+  // If the product has dimensions, display them in a table.
+  if ($dimensions) {
+    echo '<h3 class="section-title">Product Dimensions</h3>';
+    echo '<table class="product_dimensions">';
+    echo '<tr>';
+    foreach ($dimensions as $dimension_name => $dimension_value) {
+      echo '<th>' . ucfirst($dimension_name) . '</th>';
+    }
+    echo '</tr>';
+    echo '<tr>';
+    foreach ($dimensions as $dimension_name => $dimension_value) {
+      echo '<td>' . esc_html($dimension_value) . '</td>';
+    }
+    echo '</tr>';
+    echo '</table>';
+  }
+}
+
+?>
